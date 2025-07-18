0bb12f9890802b8afe36690207a4d5e2954bdf14
Mayur Patil <130137417+mayurvij22@users.noreply.github.com>
2025-07-18 22:49:28 +0530

    Update Navbar.jsx

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -4,7 +4,7 @@ function Navbar() {
   return (
     <header className="w-full bg-indigo-700 text-white shadow-md py-4 px-6">
       <div className="flex flex-col md:flex-row md:justify-between md:items-center">
-        <h1 className="text-2xl font-bold tracking-wider">mayoZ</h1>
+        <h1 className="text-2xl font-bold tracking-wider">mayorZ</h1>
 
         <div className="mt-2 md:mt-0">
           <span className="bg-yellow-300 text-black px-3 py-1 rounded-md text-sm font-semibold animate-pulse">
