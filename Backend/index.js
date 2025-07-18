dcfe723ad94ddbf66d1cb7d4cc978f3e898f41a4
Mayur Patil <mayurvij22@gmail.com>
2025-07-18 19:53:40 +0530

    Express

--- Backend/index.js
+++ Backend/index.js
@@ -4,7 +4,7 @@ const http = require("http");
 const socketIo = require("socket.io");
 const cors = require("cors");
 const axios = require("axios");
-
+const PORT = process.env.PORT || 5000;
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server, {
@@ -64,5 +64,4 @@ async function getAIResponse(prompt) {
 }
 
 
-const PORT = 5000;
 server.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
